d9bf9694faf4e1d58143c8f30998bfdfa8609a2a
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-07-15 12:37:45 +0300

    added more requests and tested some stuff

--- server-side/src/controller.js
+++ server-side/src/controller.js
@@ -1,13 +1,39 @@
 const pool = require('../db');
+const queries = require("./queries")
 
 const getPolicies = (req, res) => {
-    pool.query("SELECT * FROM politici", (error, results) => {
+    pool.query(queries.getPoliciesWithoutId, (error, results) => {
         if(error) throw error;
 
         res.status(200).json(results.rows);
     })
 }
 
+const getPolicyById = (req, res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getPolicyById, [id], (error, results) => {
+        if(error) throw error;
+
+        if(results.rows.length < 1) {
+            res.status(404).json("Policy not found");
+        } else {
+            res.status(200).json(results.rows);
+        }
+    })
+}
+
+const addPolicy = (req, res) => {
+    const { nume, module_active, editable } = req.body;
+
+    pool.query(queries.addNewPolicy, [nume, module_active, editable], (error, results) => {
+        if(error) throw error;
+
+        res.status(201).json("New policy created");
+    })
+}
+
 module.exports = {
     getPolicies,
+    getPolicyById,
+    addPolicy,
 };
--- server-side/src/routes.js
+++ server-side/src/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const controller = require("./controller")
+const controller = require("./controller");
 
 const router = Router();
 
-router.get('/', controller.getPolicies)
+router.get("/getPolicies", controller.getPolicies);
+router.get("/getPolicyById/:id", controller.getPolicyById);
+router.post("/addPolicy", controller.addPolicy);
 
 module.exports = router;
